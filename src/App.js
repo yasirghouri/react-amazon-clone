08290c5c742403f29edbee76b137f92869bc1bdb
yasirghouri <yasir.ag7@gmail.com>
2021-01-18 11:58:39 +0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Switch, Route } from "react-router-dom";
+
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Switch>
+        <Route path="/checkout">
+          <h1>Checkout</h1>
+        </Route>
+        <Route path="/login">
+          <h1>Login</h1>
+        </Route>
+        <Route path="/">
+          <Header />
+          <h1>Homepage</h1>
+        </Route>
+      </Switch>
     </div>
   );
 }
