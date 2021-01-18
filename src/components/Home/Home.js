b26144fe090a6464fe92ddeee73e513f92fba693
yasirghouri <yasir.ag7@gmail.com>
2021-01-18 13:41:00 +0500

    added product component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Product from "../Product/Product";
 import "./Home.css";
 
 const Home = () => {
@@ -9,6 +10,54 @@ const Home = () => {
         src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/gateway/placement/launch/VikingsS6B/VIKIN_S6_GWBleedingHero_FT_COVIDUPDATE_XSite_1500X600_PV_en-GB._CB411502696_.jpg"
         alt="banner"
       />
+      <div className="home__row">
+        <Product
+          id={1}
+          title="HP 63 | Ink Cartridge | Black | F6U62AN"
+          image="https://images-na.ssl-images-amazon.com/images/I/71QkoRlulgL._AC_SL1500_.jpg"
+          price={30.86}
+          rating={5}
+        />
+        <Product
+          id={2}
+          title="Logitech C920x Pro HD Webcam"
+          image="https://images-na.ssl-images-amazon.com/images/I/71iNwni9TsL._AC_SL1500_.jpg"
+          price={91.49}
+          rating={3}
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id={3}
+          title="Olaplex Hair Perfector No 3 Repairing Treatment"
+          image="https://images-na.ssl-images-amazon.com/images/I/71gnxxwxAXL._SL1500_.jpg"
+          price={28.01}
+          rating={4}
+        />
+        <Product
+          id={4}
+          title="Coty Airspun Loose Face Powder, Translucent, Pack of 1"
+          image="https://images-na.ssl-images-amazon.com/images/I/71le2ZYj97L._SL1250_.jpg"
+          price={5.96}
+          rating={4}
+        />
+        <Product
+          id={5}
+          title="Bath and Body Works Teakwood Men's Collection 3.4 Ounce Cologne Spray New In Box"
+          image="https://images-na.ssl-images-amazon.com/images/I/41zltclralL._SL1000_.jpg"
+          price={44.3}
+          rating={5}
+        />
+      </div>
+      <div className="home__row">
+        <Product
+          id={6}
+          title="Acer SB220Q bi 21.5 Inches Full HD (1920 x 1080) IPS Ultra-Thin Zero Frame Monitor (HDMI & VGA Port), Black"
+          image="https://images-na.ssl-images-amazon.com/images/I/81QpkIctqPL._AC_SL1500_.jpg"
+          price={93.99}
+          rating={4}
+        />
+      </div>
     </div>
   );
 };
---
+++ src/components/Product/Product.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Product.css";
+import StarIcon from "@material-ui/icons/Star";
+
+const Product = ({ id, title, image, rating, price }) => {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => {
+              return (
+                <p key={i}>
+                  <StarIcon className="product__ratingStar" />
+                </p>
+              );
+            })}
+        </div>
+      </div>
+      <img src={image} className="product__image" alt="" />
+      <button className="product__button">Add To Basket</button>
+    </div>
+  );
+};
+
+export default Product;
